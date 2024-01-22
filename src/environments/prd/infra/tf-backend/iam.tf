46dc0327aac9236b9f436409a41755f5a0f665dc
Roel de Cort <roel.de.cort@simac.com>
2024-01-22 23:26:43 +0100

    Refactor Terraform backend configuration and IAM policies

---
+++ src/environments/prd/infra/tf-backend/iam.tf
@@ -0,0 +1,49 @@
+data "aws_iam_user" "main" {
+  user_name = local.iam_user_name
+}
+
+data "aws_iam_policy_document" "main" {
+  for_each = local.iam_policies
+
+  dynamic "statement" {
+    for_each = each.value.statements
+    content {
+      sid    = try(statement.value.sid, null)
+      effect = statement.value.effect
+
+      dynamic "principals" {
+        for_each = statement.value.principals
+        content {
+          type        = principals.value.type
+          identifiers = [principals.value.identifier]
+        }
+      }
+
+      actions   = statement.value.actions
+      resources = statement.value.resources
+    }
+  }
+}
+
+resource "aws_iam_role" "main" {
+  for_each = local.iam_roles
+
+  name               = each.value.name
+  description        = each.value.description 
+  assume_role_policy = data.aws_iam_policy_document.main[each.value.assume_role_policy].json
+}
+
+resource "aws_iam_policy" "main" {
+  for_each = local.iam_roles
+
+  name        = each.value.name
+  description = each.value.description
+  policy      = data.aws_iam_policy_document.main[each.value.policy_to_attach].json
+}
+
+resource "aws_iam_role_policy_attachment" "main" {
+  for_each = local.iam_roles
+
+  role       = aws_iam_role.main[each.value.role].name
+  policy_arn = aws_iam_policy.main[each.value.policy_to_attach].arn
+}
--- src/environments/prd/infra/tf-backend/locals.tf
+++ src/environments/prd/infra/tf-backend/locals.tf
@@ -1,5 +1,6 @@
 locals {
   iam_user_name  = "dev-terraform"
+
   s3_config_yaml = yamldecode(file("_configuration/tf_state_s3.yaml")).s3_config
 
   s3_config = {
@@ -10,6 +11,45 @@ locals {
       versioning       = value.versioning
       object_ownership = value.object_ownership
       policy           = data.aws_iam_policy_document.main[key].json
+    }
+  }
+
+  iam_policy_yaml = yamldecode(templatefile("_configuration/tf_state_s3.yaml"), {    
+    user_arn            = data.aws_iam_user.main.arn
+    bucket_name         = each.value.bucket
+    region_name         = data.aws_region.current.name
+    account_id          = data.aws_caller_identity.current.account_id
+    dynamodb_table_name = aws_dynamodb_table.main.name
+  }).iam_config.policies
+
+  iam_policies = {
+    for key, value in local.iam_policy_yaml :
+    key => {
+      statements = [
+        {
+          sid       = value.sid
+          effect    = value.effect
+          principals = can(value.principal_type) && can(value.principal_identifier) ? [
+            {
+              type       = value.principal_type
+              identifier = value.principal_identifier
+            }
+          ] : []
+          actions   = value.actions
+          resources = value.resources
+        }
+      ]
+    }
+  }
+
+  iam_role_yaml = yamldecode(file("_configuration/tf_state_s3.yaml")).iam_config.roles
+
+  iam_roles = {
+    for key, value in local.iam_role_yaml :
+    key => {
+      name               = value.name
+      description        = value.description
+      assume_role_policy = value.assume_role_policy
     }
   }
 }
